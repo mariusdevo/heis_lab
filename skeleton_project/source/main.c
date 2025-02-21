07cd4d72ef9a02c55ecc97abd3c243397caa05cd
henrikOes <henrik.ostvik@gmail.com>
2025-02-21 12:40:46 +0100

    test

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -8,7 +8,7 @@
 
 int main(){
     elevio_init();
-    //hello
+    //hello bitch
     printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
     elevio_motorDirection(DIRN_DOWN);
